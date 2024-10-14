954f6a50cc27fae11c74132279fcd900ee9eea23
simushy <simushy@gmail.com>
2024-10-15 01:09:37 +0200

    Dodaj pliki projektów.

---
+++ MauiApp1/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿namespace MauiApp1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            CreateMainPage();
+        }
+
+        private void CreateMainPage()
+        {
+            var mainPage = new NavigationPage(new MainPage());
+
+            // Ensure the window is initialized properly
+            this.Dispatcher.Dispatch(() =>
+            {
+                if (mainPage.Window != null)
+                {
+                    mainPage.Window.MaximumWidth = 280;
+                    mainPage.Window.MaximumHeight = 400;
+                    mainPage.Window.MinimumWidth = 280;
+                    mainPage.Window.MinimumHeight = 400;
+                }
+            });
+
+            MainPage = mainPage;
+
+        }
+    }
+}
---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿
+
+namespace MauiApp1
+{
+    public class Unit
+    {
+        public string name { get; set; }
+        public int value { get; set; }
+    }
+    public partial class MainPage : ContentPage
+    {
+        public List<Unit> Units { get; set; }
+        public Unit ConvertUnitFrom { get; set; }
+        public Unit ConvertUnitTo { get; set; }
+        public string Value { get; set; }
+        private string resultvaluetext;
+        public string ResultValueText
+        { 
+            get { return resultvaluetext; } 
+            set { resultvaluetext = value; OnPropertyChanged(); }
+        }
+
+        public MainPage()
+        {
+            Units = new List<Unit>();
+            Units.Add(new Unit { name = "mm", value = 1 });
+            Units.Add(new Unit { name = "cm", value = 10 });
+            Units.Add(new Unit { name = "dm", value = 100});
+            Units.Add(new Unit { name = "m", value = 1000});
+            Units.Add(new Unit { name = "km", value = 1000000 });
+            InitializeComponent();
+        }
+        private void OnClick(object sender, EventArgs e)
+        {
+            double ResultValue = double.Parse(Value);
+            ResultValue = ResultValue * ConvertUnitFrom.value;
+            ResultValue = ResultValue / ConvertUnitTo.value;
+            int Accuracy = (ConvertUnitTo.value / ConvertUnitFrom.value).ToString().Length-1;
+            ResultValueText = $"{Value} {ConvertUnitFrom.name} is {ResultValue.ToString($"F{Accuracy}")} {ConvertUnitTo.name}";
+
+        }
+    }
+    
+
+}

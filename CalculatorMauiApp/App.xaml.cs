32c37244d4a1fe4cb28091915b5822cc3365b2b1
q <q>
2024-10-22 14:24:58 +0200

    bajojajokalkulator

---
+++ CalculatorMauiApp/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace CalculatorMauiApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            CreateMainPage();
+        }
+        private void CreateMainPage()
+        {
+            var mainPage = new NavigationPage(new MainPage());
+
+            // Ensure the window is initialized properly
+            this.Dispatcher.Dispatch(() =>
+            {
+                if (mainPage.Window != null)
+                {
+                    mainPage.Window.MaximumWidth = 700;
+                    mainPage.Window.MaximumHeight = 800;
+                    mainPage.Window.MinimumWidth = 700;
+                    mainPage.Window.MinimumHeight = 800;
+                }
+            });
+
+            MainPage = mainPage;
+
+        }
+    }
+}
---
+++ CalculatorMauiApp/CalculatorViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorMauiApp
+{
+    public class CalculatorViewModel : BindableObject
+    {
+        private string calculatingResult = "";
+        private Command numberCommand;
+        public Command NumberCommand
+        {
+            get
+            {
+                if (numberCommand == null)
+                {
+                    numberCommand = new Command<string>((string number) =>
+                    {
+                        CalculatingResult = CalculatingResult + number;
+                    });
+                }
+                return numberCommand;
+
+            }
+            set { numberCommand = value; }
+        }
+        public string CalculatingResult
+        {
+            get { return calculatingResult; }
+            set
+            {
+                calculatingResult = value;
+                OnPropertyChanged();
+            }
+        }
+        public CalculatorViewModel()
+        {
+
+
+        }
+    }
+}
